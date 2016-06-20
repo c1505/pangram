c035f51bada5cbc7217c9c8ffe0c2b6a480594bb
c1505 <coreym247@gmail.com>
2016-06-20 17:22:01 +0000

    done

---
+++ pangram.rb
@@ -0,0 +1,18 @@
+# make it work.  make it right.  make it fast
+   module BookKeeping
+     VERSION = 2 
+   end
+class Pangram
+    ALPHABET = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+    def self.is_pangram?(str)
+        str_array = str.downcase.split("")
+        result = true
+        ALPHABET.each do |letter|
+            unless str_array.include?(letter)
+                result = false
+                break
+            end
+        end
+        result
+    end
+end
